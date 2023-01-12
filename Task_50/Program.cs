c298f0a26acccbdae098736f9f60957adb751bc6
Zhenya <ndzhenya@mail.ru>
2023-01-12 22:38:44 +0300

    Task_50

---
+++ Task_50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая на вход принимает число,
+// и возвращает индексы числа в двумерном массиве или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillNewArr (int[,] NewArr)
+
+{
+for (int i = 0; i < NewArr.GetLength(0); i++)
+{
+    for (int j = 0; j < NewArr.GetLength(1); j++)
+    {
+        NewArr[i, j] = new Random().Next(1, 10);
+    }
+}
+return NewArr;
+}
+void PrintNewArr (int[,] NewArr)
+{
+for (int i = 0; i < NewArr.GetLength(0); i++)
+ {
+     for (int j = 0; j < NewArr.GetLength(1); j++)
+     {
+       Console.Write(NewArr[i, j] + " ");
+     }
+     Console.WriteLine();
+ }
+}
+
+int[,] NewArr = new int[3,4];
+NewArr = FillNewArr(NewArr);
+PrintNewArr(NewArr);
+
+Console.Write("\n Введите  пожалуйста число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.Write("\n"); 
+bool numberInArr = false; 
+
+for (int i = 0; i < NewArr.GetLength(0); i++)
+{
+    for (int j = 0; j < NewArr.GetLength(1); j++)
+    {
+         if (NewArr[i,j] == number) 
+        { 
+            numberInArr = true;
+            Console.WriteLine($"[{Convert.ToInt32(i)},{Convert.ToInt32(j)}]"); 
+        } 
+    } 
+} 
+if (numberInArr) 
+    {}
+else 
+    { 
+        Console.Write($"Такого числа нет!"); 
+    }
